ec643229edf2f945889b8ac420e54056ddea7077
emptywood <45452376+emptywood@users.noreply.github.com>
2021-05-25 21:19:20 +0900

    神経衰弱

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-$(function() {
-  $("h1").on("click",function(){
-    $("p").css("color","#f00");
+$(function(){
+  let kigo = ['!','#','$','%','&','=','@','+','!','#','$','%','&','=','@','+'];
+  let sels = [];
+  
+  //配列のシャッフル
+  for(let i=0; i<20; i++){
+    let r1 = Math.floor(Math.random() * 16);
+    let r2 = Math.floor(Math.random() * 16);
+    let tmp = kigo[r1];
+    kigo[r1] = kigo[r2];
+    kigo[r2] = tmp; 
+  }
+  //カードの配布
+  for(let i=0; i<16; i++){
+    let ko = $("<div>").text(kigo[i]);
+    $("#container").append(ko);
+  }
+  //クリック時イベントの登録
+  $("#container div").on("click",function(){
+    $(this).css("color","#fff");
+    let moji = $(this).text();
+    sels.push(moji);
+    if(sels.length == 2){
+      //比較判定
+      if(sels[0] == sels[1]){
+        $("#msg").text("OK!");
+      }else{
+        $("#msg").text("NG!");
+      }
+      sels = [];//初期化
+    }
   });
-  $(".hoge").text("ほげじゃありません");
-  $("#btn").on("click",function(){
-    let str=$("#t1").val();
-    alert(str);
-  });
-  $("#t1").attr("type","checkbox");
 });
